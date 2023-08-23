c3553ec4b74bbf5dd51ae66c77a94838b7217879
iyoussef <ibraheemyoussefiy@gmail.com>
2023-08-23 14:30:21 +0100

    Added print functionality to site

--- main.go
+++ main.go
@@ -2,17 +2,50 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+type Formdata struct {
+	data   string
+	banner string
+}
+
 func main() {
+
 	fileserver := http.FileServer(http.Dir("./templates"))
+
 	http.Handle("/", fileserver)
+	http.HandleFunc("/process", handleInput)
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
+
+}
+
+func handleInput(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, "Error parsing form data", http.StatusBadRequest)
+		return
+	}
+
+	value := r.FormValue("fname")
+	banner := r.FormValue("styles")
+	valueStruct := Formdata{
+		data:   value,
+		banner: banner,
+	}
+
+	tpl, err := template.ParseFiles("./templates/index.html")
+	if err != nil {
+		http.Error(w, "Error rendering template", http.StatusInternalServerError)
+		return
+	}
+
+	tpl.Execute(w, valueStruct)
+	fmt.Fprintf(w, "%s ,%s", valueStruct.data, valueStruct.banner)
 
 }
